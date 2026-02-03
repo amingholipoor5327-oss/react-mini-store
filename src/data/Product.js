4485ed4aaa767502d1e204390b26336490f96945
amin <amingholipoor5327@gamil.com>
2026-02-03 16:41:34 +0330

    data-pro

---
+++ src/data/Product.js
@@ -0,0 +1,52 @@
+import camera from  "./accsent/camera.webp"
+import dasti from  "./accsent/dasti.jpg"
+import dorbin from  "./accsent/dorbin.jpg"
+import images from  "./accsent/images.jpg"
+import IPC from  "./accsent/IPc.jpg"
+import lightbox from  "./accsent/lightbox.jpg"
+import Phone from  "./accsent/Phone.jpg"
+
+export const Product =[
+    {
+        productName: "camera",
+        ProductImage: camera,
+        prise: "1000$",
+        id: 1
+    },
+     {
+        productName: "dasti",
+        ProductImage: dasti,
+        prise: "2000$",
+        id: 2
+    },
+     {
+        productName: "dorbin",
+        ProductImage: dorbin,
+        prise: "3000$",
+        id: 3
+    },
+     {
+        productName: "images",
+        ProductImage: images,
+        prise: "4000$",
+        id: 4
+    },
+     {
+        productName: "IPC",
+        ProductImage: IPC,
+        prise: "5000$",
+        id: 5
+    },
+     {
+        productName: "lightbox",
+        ProductImage: lightbox,
+        prise: "6000$",
+        id: 6
+    },
+     {
+        productName: "phone",
+        ProductImage: Phone,
+        prise: "7000$",
+        id: 7
+    }
+]
